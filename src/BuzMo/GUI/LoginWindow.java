b83b754297e865f841f77a9a4e384f12f67c57d3
Ben Lei <benjamin.g.lei@gmail.com>
2016-12-01 04:46:38 -0800

    Made edits to LoginWindow and MainMenu

--- src/BuzMo/GUI/LoginWindow.java
+++ src/BuzMo/GUI/LoginWindow.java
@@ -4,8 +4,8 @@ import BuzMo.Database.DatabaseException;
 import BuzMo.Database.User;
 import BuzMo.Logger.Logger;
 import javax.swing.*;
-import java.awt.event.*;
 import java.sql.Connection;
+import java.util.Scanner;
 
 import static java.lang.System.exit;
 
@@ -22,27 +22,35 @@ public class LoginWindow extends JFrame {
         this.log = log;
         this.connection = connection;
 
+        Scanner userInput = new Scanner(System.in);
+
+        String usernameInput;
+        System.out.print("Enter username (email): ");
+        usernameInput = userInput.next();
+
+        String passwordInput;
+        System.out.print("Enter password: ");
+        passwordInput = userInput.next();
+
         try{
-            Boolean exists = User.exists(connection,usernameInput);
+            Boolean exists = User.exists(connection, usernameInput);
 
             //Check if user exists
             if(!exists){
-
+                log.Log("LoginWindow -- Invalid username entered!");
             }
 
-            // ADD SQL QUERY to look up email and password in Users table.
             // If login successful, bring up a new Main Menu and dispose of the current window
-            String pass = User.getPassword(log, connection,usernameInput);
+            String pass = User.getPassword(log, connection, usernameInput);
 
             if (passwordInput.equals(pass)) {
                 new MainMenu(log, usernameInput);
             }
-            // If username or password are incorrect, ask for it again
             else {
-                
-            } catch(Exception except){
-                log.Log("Error occurred in login window "+except.getMessage());
+                log.Log("LoginWindow -- Invalid password entered!");
             }
+        } catch(Exception except){
+            log.Log("LoginWindow -- Error: "+except.getMessage());
         }
 
         log.Log("GUI -- LoginWindow properly loaded");
